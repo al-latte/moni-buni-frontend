66ef9206762a3827daec0b77660d0a653341a520
al-latte <robertskarlisha@gmail.com>
2025-03-16 11:51:31 -0400

    remove unwanted code

--- src/features/transactions/components/TransactionItem.tsx
+++ src/features/transactions/components/TransactionItem.tsx
@@ -10,7 +10,7 @@ import {
   useTransform,
   useDragControls,
 } from "motion/react";
-import { AlertDialog, AlertDialogAction, AlertDialogCancel, AlertDialogContent, AlertDialogDescription, AlertDialogFooter, AlertDialogHeader, AlertDialogTitle, AlertDialogTrigger } from "@/components/ui/alert-dialog";
+import { AlertDialog, AlertDialogAction, AlertDialogCancel, AlertDialogContent, AlertDialogDescription, AlertDialogFooter, AlertDialogHeader, AlertDialogTitle } from "@/components/ui/alert-dialog";
 import { useState } from "react";
 
 interface TransactionItemProps {
